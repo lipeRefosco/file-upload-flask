3103301df996902b4e1e7767c88a6d2f1ffa84f8
lipeRefosco <luisfelipe.refosco@hotmail.com>
2022-06-19 23:35:57 -0300

    Escape zip file or directory

--- client/main.py
+++ client/main.py
@@ -7,43 +7,43 @@ date = datetime.datetime.now()
 date_format = str(date.year) + '-' + str(date.month) + '-' + str(date.day)
 hour_format = str(date.hour) + ':' + str(date.minute) + ':' + str(date.second)
 
-DEFAULT_USER_FOLDER = os.path.expanduser("~")
-DEFAULT_FOLDER_NAME = "raioss"
-DEFAULT_FOLDER      = DEFAULT_USER_FOLDER + DEFAULT_FOLDER_NAME
-DEFAULT_ZIP_NAME    = f"{DEFAULT_FOLDER_NAME}_-_{date_format}_{hour_format}.zip"
+DEFAULT_USER_DIRECTORY = os.path.expanduser("~")
+DEFAULT_DIRECTORY_NAME = "raioss"
+DEFAULT_DIRECTORY      = DEFAULT_USER_DIRECTORY + DEFAULT_DIRECTORY_NAME
+DEFAULT_ZIP_NAME    = f"{DEFAULT_DIRECTORY_NAME}_-_{date_format}_{hour_format}.zip"
 
 def main():
 
-    go_default_folder(DEFAULT_USER_FOLDER, DEFAULT_FOLDER_NAME)
+    go_default_directory(DEFAULT_USER_DIRECTORY, DEFAULT_DIRECTORY_NAME)
     
     zip_all_files(DEFAULT_ZIP_NAME)
 
     return
 
-def go_default_folder( user_folder : str, name_folder : str ):
+def go_default_directory( user_directory : str, name_directory : str ):
 
-    if os.path.exists(user_folder + os.path.sep + name_folder):
-        os.chdir(user_folder + os.path.sep + name_folder)
+    if os.path.exists(user_directory + os.path.sep + name_directory):
+        os.chdir(user_directory + os.path.sep + name_directory)
         return
     else:
-        os.chdir(user_folder)
-        os.mkdir(name_folder)
-        go_default_folder(user_folder, name_folder)
+        os.chdir(user_directory)
+        os.mkdir(name_directory)
+        go_default_directory(user_directory, name_directory)
 
-def zip_all_files(output_name : str, folder : str = "."):
-    # Zip all files from folder.
+def zip_all_files(output_name : str, directory : str = "."):
+    # Zip all files from directory.
 
     # List of files
     files = os.listdir()
 
     # Create a zip file
-    with ZipFile(f"{folder}{os.path.sep}{output_name}", 'w') as zip:
+    with ZipFile(f"{directory}{os.path.sep}{output_name}", 'w') as zip:
         
         print(f"Creating zip file \"{output_name}\"!")
 
         for file in files:
-            # Escape zip file
-            if '.zip' in file:
+            # Escape zip file or directory
+            if '.zip' in file or os.path.isdir(file):
                 continue
             
             print(f"Zipping file \"{file}\"")
