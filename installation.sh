d56fc62ff46cf319108bc9c113f2586d171ca8b1
lipeRefosco <luisfelipe.refosco@hotmail.com>
2022-06-22 07:45:53 -0300

    Installation script

---
+++ installation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Installing apache2"
+apt install apache2
+
+echo "Installing python3 python3-pip"
+apt install python3 python3-pip
+
+echo "Intalling Flask with pip"
+pip install Flask
+
+echo "Coping 'server' folder into /var/www/html"
+cp -rfv ./server /var/www/html
+
+echo "Coping file '000-default.config' into /etc/apache2/sites-available/" 
+cp ./infra/000-default.conf
+
+echo "Reloading apache2"
+service apache2 reload
