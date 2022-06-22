474099aa8949a3f0431efb52389821179e3889ff
lipeRefosco <luisfelipe.refosco@hotmail.com>
2022-06-22 10:26:32 -0300

    Nova rota para consulta do banco de dados

---
+++ server/services/Datas.py
@@ -0,0 +1,19 @@
+import os
+from flask import Request, Response, jsonify
+import sqlite3
+
+class Datas:
+    def run(request : Request, app):
+
+        db_folder = app.config["DB_FOLDER"]
+        db_file   = app.config["DB_FILE"]
+
+        if not os.path.exists( db_folder ):
+            return jsonify("Nenhum arquivo enviado!")
+
+        con = sqlite3.connect( db_folder + os.path.sep + db_file )
+        cur = con.cursor()
+
+        db_data = cur.execute("SELECT * FROM uploads")
+
+        return jsonify(db_data.fetchall())
