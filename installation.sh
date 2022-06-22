1abce20d3c54b07ec4ee5f3f0dd8b5b34d370bff
lipeRefosco <luisfelipe.refosco@hotmail.com>
2022-06-22 07:48:43 -0300

    Updating destination folder for the apache folder

--- installation.sh
+++ installation.sh
@@ -13,7 +13,7 @@ echo "Coping 'server' folder into /var/www/html"
 cp -rfv ./server /var/www/html
 
 echo "Coping file '000-default.config' into /etc/apache2/sites-available/" 
-cp ./infra/000-default.conf
+cp ./infra/000-default.conf /etc/apache2/sites-available/
 
 echo "Reloading apache2"
 service apache2 reload
