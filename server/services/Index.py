9272b647f414723248c432e2079a0bd7a36b7d81
lipeRefosco <luisfelipe.refosco@hotmail.com>
2022-06-22 10:43:17 -0300

    Novo home

--- server/services/Index.py
+++ server/services/Index.py
@@ -1,5 +1,13 @@
 
+from flask import jsonify
+
+
 class Index:
     
     def run():
-        return "Index page"
+        return jsonify({
+            "/"     : "Esta página!",
+            "/ip"   : "Retorna o Ip do client!",
+            "/datas": "Retorna os dados já gravados no banco!",
+            "upload": "Rota de upload dos arquivos, apenas no metodo POST!"
+        })
