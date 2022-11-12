df06eb490c96a0f94bb0379953eab36dbb6a4a66
lipeRefosco <luisfelipe.refosco@hotmail.com>
2022-11-12 11:46:49 -0300

    style: renaming db name

--- server/init.py
+++ server/init.py
@@ -1,32 +1,32 @@
 from pathlib import Path 
 from flask import Flask, request
 from flask_cors import CORS
-import services
+from services import Index, Upload, Ip, Datas
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['APACHE_FILES_FOLDER'] = str(Path(__file__).parent.resolve()) + "/"
+app.config['FILES_FOLDER'] = str(Path(__file__).parent.resolve()) + "/"
 app.config['ALLOWED_EXTENSIONS']  = {"zip"}
-app.config['UPLOAD_FOLDER']       = app.config['APACHE_FILES_FOLDER'] + "uploads"
-app.config['DB_FOLDER']           = app.config['APACHE_FILES_FOLDER'] + "database"
-app.config['DB_FILE']             = "uploads.db"
+app.config['UPLOAD_FOLDER']       = app.config['FILES_FOLDER'] + "uploads"
+app.config['DB_FOLDER']           = app.config['FILES_FOLDER'] + "database"
+app.config['DB_FILE']             = "database.db"
 
 @app.route("/", methods=["GET"])
 def index():
-    return services.Index.run()
+    return Index.run()
 
 @app.route("/upload", methods=["POST"])
 def upload():
-    return services.Upload.run(request, app)
+    return Upload.run(request, app)
 
 @app.route("/ip", methods=["GET"])
 def ip():
-    return services.Ip.run(request)
+    return Ip.run(request)
 
 @app.route("/datas", methods=["GET"])
 def datas():
-    return services.Datas.run(request, app)
+    return Datas.run(request, app)
 
 if __name__ == "__main__":
     app.run()
