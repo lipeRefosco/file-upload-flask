a8ff3f1b1986a917435203288d92a9aff1789c5d
lipeRefosco <luisfelipe.refosco@hotmail.com>
2022-11-13 23:37:10 -0300

    test[Upload]: adicionando testes para os metodos de Upload

--- server/tests/test_Upload.py
+++ server/tests/test_Upload.py
@@ -1,4 +1,7 @@
 from services import Upload
+from zipfile import ZipFile
+import tempfile
+
 class TestUpload:
 
     def test_deve_retornar_true_quando_passado_as_extensoes_esperadas(self):
@@ -8,13 +11,15 @@ class TestUpload:
         }
         expected = True
 
-        result = Upload.allowed_file(
+        processed = Upload.allowed_file(
                 inputs.get("filename"),
                 inputs.get("extensions")
             )
+         
+        result = processed == expected
+   
+        assert result
 
-        assert result == expected
-    
     def test_deve_retornar_false_quando_não_for_as_extensoes_experadas(self):
         inputs = {
             "filename": "filenameTest.png",
@@ -22,11 +27,33 @@ class TestUpload:
         }
         expected = False
 
-        result = Upload.allowed_file(
+        processed = Upload.allowed_file(
             inputs.get("filename"),
             inputs.get("extensions")
         )
+        
+        result = processed == expected
+        
+        assert result
 
-        assert result == expected
+    def test_deve_recuperar_a_data_do_nome_do_arquivo(self):
+        inputs = "nomearquivo_-_10-10-22_-_00-00.zip"
 
-    
+        expected = "10-10-22"
+
+        processed = Upload.get_date(inputs)
+        
+        result = processed == expected
+        
+        assert result
+
+    def test_deve_recuperar_e_formatar_a_hora_e_os_minutos_do_nome_do_arquivo(self):
+        inputs = "nomearquivo_-_10-10-22_-_00-00.zip"
+        expected = "00:00"
+
+        processed = Upload.get_hours(inputs)
+        
+        result = processed == expected
+        
+        assert result
+        
